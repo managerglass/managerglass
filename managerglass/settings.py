05ba10b2d019adb046f5cb07cd5a4ff7d9ea5e1c
Jesse <jessenfaria@gmail.com>
2022-01-22 09:59:58 -0300

    teste

--- managerglass/settings.py
+++ managerglass/settings.py
@@ -30,13 +30,13 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'adminlte3',
+    #'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'adminlte3_theme',
     'core',
     'bootstrap4',
     'stdimage',
