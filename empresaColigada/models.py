fc957590f9ccb784eb7275abcee3eed6a8cd3f69
GEOVANE F CAETANO <geovanecaetano@gmail.com>
2022-01-22 09:38:33 -0300

    Alteração Models Produtos - 01.004

--- empresaColigada/models.py
+++ empresaColigada/models.py
@@ -9,7 +9,7 @@ from core.models import Base
 class Empresa(Base, models.Model):
     cnpj = models.CharField('Cnpj', max_length=32, null=True, blank=True)
     nome_fantasia = models.CharField('Nome Fantasia',max_length=255, null=True, blank=True)
-    matriz = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    matriz_coligada = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     responsavel = models.CharField('Responsável',max_length=32, null=True, blank=True)
     email = models.CharField('E-mail', max_length=120, null=True, blank=True)
 
